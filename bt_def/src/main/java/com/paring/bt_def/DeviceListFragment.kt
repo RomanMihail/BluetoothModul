431e67c0131b25d7b8b32f8fd9ee6b78d8f5d494
RomanMihail <roman.mihail.p@gmail.com>
2023-05-02 17:30:53 +0300

    Соединение блютуз устройств со смартфоном.

--- bt_def/src/main/java/com/paring/bt_def/DeviceListFragment.kt
+++ bt_def/src/main/java/com/paring/bt_def/DeviceListFragment.kt
@@ -66,8 +66,8 @@ class DeviceListFragment : Fragment(), ItemAdapter.Listener {
     private fun initRcView() = with(binding){
         rcViewPaired.layoutManager = LinearLayoutManager(requireContext())
         rcViewSearch.layoutManager = LinearLayoutManager(requireContext())
-        itemAdapter = ItemAdapter(this@DeviceListFragment)
-        discoveryAdapter = ItemAdapter(this@DeviceListFragment)
+        itemAdapter = ItemAdapter(this@DeviceListFragment, false)
+        discoveryAdapter = ItemAdapter(this@DeviceListFragment, true)
         rcViewPaired.adapter = itemAdapter
         rcViewSearch.adapter = discoveryAdapter
     }
@@ -165,13 +165,14 @@ class DeviceListFragment : Fragment(), ItemAdapter.Listener {
                 list.addAll(discoveryAdapter.currentList)
                 if (device != null) list.add(ListItem(device,false))
                 discoveryAdapter.submitList(list.toList())
+                binding.tvEmptySearch.visibility = if (list.isEmpty()) View.VISIBLE else View.GONE
                 try {
                     Log.d("MyLog", "Device: ${device?.name}")
                 }catch (e: SecurityException){
 
                 }
             }else if (intent?.action == BluetoothDevice.ACTION_BOND_STATE_CHANGED){
-
+                getPairedDevices()
             }else if (intent?.action == BluetoothAdapter.ACTION_DISCOVERY_FINISHED){
 
             }
